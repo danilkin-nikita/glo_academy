1611c3a68518e3ebbd09e7f24d0e4001bb0d1e29
Nikita Danilkin <ni.danilkin@gmail.com>
2020-11-20 17:30:47 +0300

    Второе задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-let money = 1000;
-let income = 700;
-let addExpenses = 300;
-let deposit = 400;
-let mission = 20;
-let period = 100;
+let money = 500;
+let income = 'фриланс';
+let addExpenses = 'интернет, такси, налоги';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Hello!');
-
-console.log('money =', money);
+console.log(typeof(money), typeof(income), typeof(deposit));
+console.log('Длина строки addExpenses =', addExpenses.length);
+console.log('Период равен', period, 'месяцев', '\nЦель заработать', mission, 'долларов');
+console.log(addExpenses.toUpperCase().split(', '));
+console.log('Доход за день', budgetDay);
